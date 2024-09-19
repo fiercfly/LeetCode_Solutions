1e2372411a95fa1447fb6b495c1e4570c88e4a18
fiercfly <121884340+fiercfly@users.noreply.github.com>
2024-09-19 20:20:30 +0530

    Time: 15 ms (13.26%), Space: 20.7 MB (66%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -2,41 +2,36 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         stack<int> st;
-        
-        for(int i : asteroids) {
-            bool broke = false;
-            
-            // Process only when stack has a positive asteroid and current one is negative
-            while(!st.empty() && st.top() > 0 && i < 0) {
-                if(abs(st.top()) < abs(i)) {
-                    // Current asteroid is larger, pop the stack and continue
-                    st.pop();
-                } else if(abs(st.top()) == abs(i)) {
-                    // They are equal, both are destroyed
-                    st.pop();
-                    broke = true;
-                    break;
-                } else {
-                    // Stack top is larger, current asteroid is destroyed
-                    broke = true;
-                    break;
+        for(int i: asteroids){
+            bool broke= false;
+            // if(!st.empty()){
+                //stack ka top pos h and num neg h
+                while(!st.empty()&& st.top()>0 &&  i<0){
+                    if(abs(st.top())< abs(i)){
+                        cout<<"k";
+                        st.pop();
+                    }
+                    else if(abs(st.top()) == abs(i)){
+                        st.pop();
+                        broke= true;
+                        break;
+                    }
+                    else{
+                        broke= true;
+                        break;
+                    }
                 }
-            }
-            
-            // If the current asteroid was not destroyed, push it to the stack
-            if(!broke) {
+            if(!broke){
                 st.push(i);
             }
         }
-        
-        // Collect remaining asteroids from stack
         vector<int> ans;
-        while(!st.empty()) {
+        while(!st.empty()){
             ans.push_back(st.top());
             st.pop();
         }
+        reverse(ans.begin(), ans.end());
         
-        reverse(ans.begin(), ans.end()); // Reverse to maintain original order
         return ans;
     }
 };
