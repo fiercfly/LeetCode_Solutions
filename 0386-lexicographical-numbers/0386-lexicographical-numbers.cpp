9336a596f8700c1f30e52712f4250e52ddbaebcf
fiercfly <121884340+fiercfly@users.noreply.github.com>
2024-09-21 17:31:43 +0530

    Time: 4 ms (88.45%), Space: 12.5 MB (86.28%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> ans;
+        int cur = 1;
+        while(ans.size() < n){
+                if(cur <= n) {
+                    ans.push_back(cur);
+                    cur = cur * 10;
+                }
+                else{
+                    cur= cur / 10;
+                    while(cur%10 == 9){
+                        cur= cur/10;
+                    }
+                    cur++;
+                }
+        }
+        return ans;
+    }
+};
+
+// 19
