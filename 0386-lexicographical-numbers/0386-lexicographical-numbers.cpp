e3ded8a5e90c97b076d1b488c963981745627cdd
fiercfly <121884340+fiercfly@users.noreply.github.com>
2024-09-21 17:33:25 +0530

    Time: 4 ms (88.45%), Space: 12.4 MB (93.86%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,10 +1,10 @@
 
-auto init = [](){ 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
+// auto init = [](){ 
+//     ios::sync_with_stdio(0);
+//     cin.tie(0);
+//     cout.tie(0);
+//     return 'c';
+// }();
 
 class Solution {
 public:
