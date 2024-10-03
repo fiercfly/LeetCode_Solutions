eb65da94af6f6d4aa782aec35f686b999964c5db
fiercfly <121884340+fiercfly@users.noreply.github.com>
2024-10-03 15:53:23 +0530

    Time: 27 ms (67.82%), Space: 45.8 MB (5.94%) - LeetHub

--- 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/3570-count-of-substrings-containing-every-vowel-and-k-consonants-i.java
+++ 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/3570-count-of-substrings-containing-every-vowel-and-k-consonants-i.java
@@ -2,26 +2,24 @@ import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
-    public int countOfSubstrings(String w, int k) {
-        Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
+    public int countOfSubstrings(String str, int k) {
         int ans = 0;
-        int n = w.length();
-
-        for (int i = 0; i < n; i++) {
-            Set<Character> f = new HashSet<>();
-            int vowelCount = 0;
-
-            for (int j = i; j < n; j++) {
-                if (vowels.contains(w.charAt(j))) {
-                    f.add(w.charAt(j));
-                    vowelCount++;
+        int n= str.length();
+        for (int i= 0; i < n; i++) {
+            Set<Character> hs= new HashSet<>();
+            int vc= 0;
+            for (int j= i; j< n;j++){
+                char ch= str.charAt(j);
+               if(ch=='a' ||ch=='e' || ch=='i'|| ch=='o'|| ch=='u'){
+                    hs.add(str.charAt(j));
+                    vc++;
                 }
-                int nonVowelCount = (j - i + 1) - vowelCount;
+                int cc=(j-i+1)-vc;
 
-                if (nonVowelCount > k) {
+                if(cc> k){
                     break;
                 }
-                if (f.size() == 5 && nonVowelCount == k) {
+                if(hs.size()== 5 &&cc== k){
                     ans++;
                 }
             }
