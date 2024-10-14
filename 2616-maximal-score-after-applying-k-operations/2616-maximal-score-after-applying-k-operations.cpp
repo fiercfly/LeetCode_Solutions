673eeb09ae770a5335829e073f238dd1d6ae1064
fiercfly <121884340+fiercfly@users.noreply.github.com>
2024-10-14 18:00:56 +0530

    Time: 135 ms (98.5%), Space: 75.8 MB (96.76%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -1,15 +1,20 @@
+
+auto init = [](){ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
 class Solution {
 public:
     long long maxKelements(vector<int>& nums, int k) {
         priority_queue<int> pq(nums.begin(), nums.end());
-
         long long ans= 0;
         while(k--){
             int x= pq.top();
-            int red= ceil(x/3.0);
-            ans+= x;
             pq.pop();
-            pq.push(red);
+            ans+= x;
+            pq.push(ceil(x/3.0));;
         }
         return ans;
     }
