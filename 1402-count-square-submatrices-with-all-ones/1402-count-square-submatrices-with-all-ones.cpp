0308f36a16bb581817915d724467067ec4b2ff49
fiercfly <121884340+fiercfly@users.noreply.github.com>
2024-10-28 00:29:54 +0530

    Time: 8 ms (39.09%), Space: 29.2 MB (6.72%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -1,4 +1,5 @@
 
+
 auto init = [](){ 
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -13,22 +14,28 @@ public:
         
         vector<vector<int>> dp(n, vector<int>(m, 0));
         int ans = 0;
+        
         //sbse pehle first row and col
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = matrix[i][0];
-            ans += dp[i][0];
-        }
-        for (int j = 1; j < m; j++) {
-            dp[0][j] = matrix[0][j];
-            ans += dp[0][j];  
-        }
+        // for (int i = 0; i < n; i++) {
+        //     dp[i][0] = matrix[i][0];
+        //     ans += dp[i][0];
+        // }
+        // for (int j = 1; j < m; j++) {
+        //     dp[0][j] = matrix[0][j];
+        //     ans += dp[0][j];  
+        // }
         //Now remaining row and cols with DP
         
-        for(int i = 1; i < n; i++) {
-            for(int j = 1; j < m; j++) {
-                if(matrix[i][j] == 1) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(matrix[i][j] == 1 && i>0 && j>0) {
                     dp[i][j] = 1 + min({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]});
                 }
+                else{
+                    dp[i][j] = matrix[i][j];
+                }
+                
+                
                 ans += dp[i][j];
             }
         }
