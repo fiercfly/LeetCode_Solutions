3f3ed7196524267986b07bc22764a35cad11a737
fiercfly <121884340+fiercfly@users.noreply.github.com>
2024-10-31 00:51:10 +0530

    Time: 63 ms (45.45%), Space: 14.8 MB (83.5%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -3,8 +3,6 @@ public:
     int minimumMountainRemovals(vector<int>& nums) {
         int n = nums.size();
         vector<int> LIS(n, 1), LDS(n, 1);
-
-        // Compute LIS up to each index
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < i; ++j) {
                 if (nums[i] > nums[j]) {
@@ -12,8 +10,6 @@ public:
                 }
             }
         }
-
-        // Compute LDS from each index
         for (int i = n - 1; i >= 0; --i) {
             for (int j = n - 1; j > i; --j) {
                 if (nums[i] > nums[j]) {
@@ -23,11 +19,9 @@ public:
         }
 
         int maxMountainLength = 0;
-
-        // Find the maximum mountain len 
         
         for (int i = 1; i < n - 1; ++i) {
-            if (LIS[i] > 1 && LDS[i] > 1) {  // Valid peak
+            if (LIS[i] > 1 && LDS[i] > 1) {  
                 maxMountainLength = max(maxMountainLength, LIS[i] + LDS[i] - 1);
             }
         }
