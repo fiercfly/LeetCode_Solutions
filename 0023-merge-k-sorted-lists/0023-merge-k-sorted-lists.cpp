a9845c542ace128224fd250620edaccaf68e6b1a
fiercfly <121884340+fiercfly@users.noreply.github.com>
2024-11-06 21:16:05 +0530

    Time: 6 ms (40.41%), Space: 19.2 MB (13.41%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -13,10 +13,9 @@ public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         ListNode* hd= new ListNode(0);
         ListNode* ans= hd;
-
         auto cmp = [](ListNode* a, ListNode* b) { return a->val > b->val; };
+        
         priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> pq(cmp);
-
         for(ListNode* l: lists){
             if(l != nullptr) pq.push(l);
         }
