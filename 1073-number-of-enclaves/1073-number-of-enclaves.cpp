8e9c69f92862bf09c7ac7a5b177ea4cba4729ba7
Akshat Jain <121884340+fiercfly@users.noreply.github.com>
2025-01-19 15:45:39 +0530

    Time: 15 ms (25.08%), Space: 38.8 MB (18.73%) - LeetHub

---
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>> &grid, int i, int j, vector<vector<int>> &vis){
+        if(i<0 || j<0 || i>=grid.size() || j>= grid[0].size() || grid[i][j]==0 || vis[i][j]){
+            return;
+        }
+
+        vis[i][j]= 1;   
+        dfs(grid, i+1, j, vis);
+        dfs(grid, i-1, j, vis);
+        dfs(grid, i, j+1, vis);
+        dfs(grid, i, j-1, vis);
+    }
+
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n= grid.size();
+        int m= grid[0].size();
+
+        vector<vector<int>> vis(n, vector<int>(m,0));
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if((i==0 || j==0 || i==n-1 || j==m-1) && grid[i][j]==1 ){
+                    dfs(grid, i, j, vis);
+                }
+            }
+        }
+
+        //counting...
+
+        int ans= 0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(!vis[i][j] && grid[i][j]==1){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+/*
+
+Apply dfs on all cells attached on boundary
+mark them as visited
+Count the remaining non visited land cells and return them
+
+*/
