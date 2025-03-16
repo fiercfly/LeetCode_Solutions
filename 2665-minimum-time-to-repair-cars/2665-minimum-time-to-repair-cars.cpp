f9b463156654e3875a693acd085fd9eb51570491
Akshat Jain <121884340+fiercfly@users.noreply.github.com>
2025-03-17 00:29:15 +0530

    Time: 79 ms (16.38%), Space: 59.4 MB (45.76%) - LeetHub

--- 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.cpp
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.cpp
@@ -1,42 +1,26 @@
 class Solution {
 public:
-    bool timeIsSuff(vector<int>& ranks, int cars, long long minGiven){
-        long long carsDone = 0;
-        for (int r : ranks) {
-            long c2 = minGiven / r;
-            long c = floor(sqrt(c2));
-            carsDone += c;
+    bool timeIsSuff(vector<int>& ranks,int cars,long long minGiven){
+        long long carsDone= 0;
+        for (int r: ranks) {
+            long c2= minGiven / r;
+            long c= floor(sqrt(c2));
+            carsDone+= c;
         } 
         return carsDone >= cars;
     }
 
 
-
-    // ______________________________________
-    // |        //      ...                 |
-    // |        //     .   .                |
-    // |        //    .     .               |
-    // |        //   .       .              |
-    // |        //  .         .             |
-    // |        // .   AkShT   .            |
-    // |        //  .  JaIn   .             |
-    // |        //   .       .              |
-    // |        //    .     .               |
-    // |        //     .   .                |
-    // |        //      ...                 |
-    // |____________________________________|
-
-
-
     long long repairCars(vector<int>& ranks, int cars){
-        long l = 1, r = 1e14;
-        while (l < r) {
-            long mid = (l + r) / 2;
-            if(timeIsSuff(ranks, cars, mid)){
-                r = mid;
+        long l= 1;
+        long r= 1e14;
+        while (l< r) {
+            long mid=(l+ r)/ 2;
+            if(timeIsSuff(ranks,cars,mid)){
+                r= mid;
             }
             else{
-                l = mid + 1;
+                l= mid+ 1;
             }
         }
         return l;
