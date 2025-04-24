6f754041fe3ae37a269994acf0d3a6c6658fbc52
Akshat Jain <121884340+fiercfly@users.noreply.github.com>
2025-04-24 17:15:45 +0530

    Time: 10 ms (92.77%), Space: 41.8 MB (59.44%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -13,35 +13,35 @@ public:
 
 //********************************************************
 
-        for(int i=0; i<nums.size(); i++){
-            unordered_map<int,int> temp;
-            int dis= 0;
-            for(int j=i; j<nums.size(); j++){
-                if(temp.find(nums[j]) == mp.end()){
-                    dis++;
-                }
-                temp[nums[j]]++;
-                if(dis == totalDistinct){
-                    ans++;
-                }
-            }
-        }
+        // for(int i=0; i<nums.size(); i++){
+        //     unordered_map<int,int> temp;
+        //     int dis= 0;
+        //     for(int j=i; j<nums.size(); j++){
+        //         if(temp.find(nums[j]) == mp.end()){
+        //             dis++;
+        //         }
+        //         temp[nums[j]]++;
+        //         if(dis == totalDistinct){
+        //             ans++;
+        //         }
+        //     }
+        // }
 
 
 //*********************************************************
 
-        // unordered_map<int,int> temp;
-        // int left = 0;
+        unordered_map<int,int> temp;
+        int left = 0;
 
-        // for(int i=0; i<nums.size(); i++){
-        //     temp[nums[i]]++;
-        //     while(temp.size() == totalDistinct){
-        //         ans+= nums.size()- i;
-        //         temp[nums[left]]--;
-        //         if(temp[nums[left]] == 0)temp.erase(nums[left]);
-        //         left++;
-        //     }
-        // } 
+        for(int i=0; i<nums.size(); i++){
+            temp[nums[i]]++;
+            while(temp.size() == totalDistinct){
+                ans+= nums.size()- i;
+                temp[nums[left]]--;
+                if(temp[nums[left]] == 0)temp.erase(nums[left]);
+                left++;
+            }
+        } 
 
 
         return ans;
