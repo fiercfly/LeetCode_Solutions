026b55893543d0ed2533fa3c7a2f7b5294a88449
Akshat Jain <121884340+fiercfly@users.noreply.github.com>
2025-05-06 16:23:29 +0530

    Time: 0 ms (100%), Space: 20.4 MB (81.05%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    vector<int> buildArray(vector<int>& nums) {
-        // unordered_map<int, int> mp;
-        vector<int> ans(nums.size(), -1);
-        for(int i=0; i<nums.size(); i++){
-            ans[i]= nums[nums[i]];
-        }
-        return ans;
-    }
+    // vector<int> buildArray(vector<int>& nums) {
+    //     // unordered_map<int, int> mp;
+    //     vector<int> ans(nums.size(), -1);
+    //     for(int i=0; i<nums.size(); i++){
+    //         ans[i]= nums[nums[i]];
+    //     }
+    //     return ans;
+    // }
 
     //Space optimised:
 
-    // vector<int> buildArray(vector<int> nums){
-    //     for(int i=0; i<nums.size(); i++)
-    //         nums[i]= (nums[i]+ 1024*((nums[nums[i]])%1024));
+    vector<int> buildArray(vector<int> nums){
+        for(int i=0; i<nums.size(); i++)
+            nums[i]= (nums[i]+ 1024*((nums[nums[i]])%1024));
 
-    //     for(int i=0; i<nums.size(); i++)
-    //         nums[i]= nums[i]/1024;
+        for(int i=0; i<nums.size(); i++)
+            nums[i]= nums[i]/1024;
 
-    //     return nums;
-    // }
+        return nums;
+    }
 
 
 };
