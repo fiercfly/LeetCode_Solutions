9b79dc07ea7eb94347697c2794fda60bbffaf23f
Akshat Jain <fiercfly@gmail.com>
2025-08-19 17:18:21 +0530

    Time: 56 ms (47.9%), Space: 61.5 MB (53.56%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    bool isPal(string s, int start, int end) {
-        while (start <= end) {
-        if (s[start++] != s[end--])
-            return false;
+    bool isPal(string s, int left, int right){
+        while(left<= right){
+            if(s[left] != s[right]) return false;
+            left++;
+            right--;
         }
         return true;
     }
-    void generate(vector<vector<string>> &ans, string s, int ind, vector<string> &path){
-        if(ind>= s.length()){
-            ans.push_back(path);
+
+    void rec(string s, vector<vector<string>> &ans, vector<string> &vct, int ind){
+        if(ind >= s.length()){
+            ans.push_back(vct);
             return;
         }
 
-        for(int i=ind; i<s.length(); i++){
-            if(isPal(s, ind, i)){
-                path.push_back(s.substr(ind, i-ind+1));
-                generate(ans,s, i+1, path);
-                path.pop_back();
+        for(int k=ind; k<s.length(); k++){
+            if(isPal(s, ind, k)){
+                vct.push_back(s.substr(ind, k-ind+1));
+                rec(s, ans, vct, k+1);
+                vct.pop_back();
             }
         }
     }
-    
+
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
-        vector<string> path;
-        generate(ans, s, 0, path);
+        vector<string> vct;
+
+        rec(s, ans, vct, 0);
+
         return ans;
     }
 };
