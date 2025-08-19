fe7ed5a462167b0bd137833bc2d2ff5f5f0d08b3
Akshat Jain <fiercfly@gmail.com>
2025-08-19 21:43:43 +0530

    Time: 0 ms (100%), Space: 10.6 MB (46.79%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,29 +1,50 @@
 class Solution {
 public:
-    void rec(vector<vector<int>> &ans, vector<int> &temp, vector<int> &freq, vector<int> &nums ){
-        if(temp.size() == nums.size()){
-            ans.push_back(temp);
-            return;
+    void rec(vector<int> &nums, vector<vector<int>> &ans, int ind){
+        if(ind >= nums.size()){
+            ans.push_back(nums);
         }
 
-        for(int i=0; i<nums.size(); i++){
-            if(!freq[i]){
-                temp.push_back(nums[i]);
-                freq[i]= 1;
-                rec(ans, temp, freq, nums);
-                freq[i]= 0;
-                temp.pop_back();
-            }
+        for(int i=ind; i<nums.size(); i++){
+            swap(nums[ind], nums[i]);
+            rec(nums,ans, ind+1);
+            swap(nums[ind], nums[i]);
         }
-
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> freq(nums.size(), 0);
-        vector<int> temp;
-
-        rec(ans, temp,freq, nums);
-
+        rec(nums, ans, 0);
         return ans;
+        
     }
 };
+
+// class Solution {
+// public:
+//     void rec(vector<vector<int>> &ans, vector<int> &temp, vector<int> &freq, vector<int> &nums ){
+//         if(temp.size() == nums.size()){
+//             ans.push_back(temp);
+//             return;
+//         }
+
+//         for(int i=0; i<nums.size(); i++){
+//             if(!freq[i]){
+//                 temp.push_back(nums[i]);
+//                 freq[i]= 1;
+//                 rec(ans, temp, freq, nums);
+//                 freq[i]= 0;
+//                 temp.pop_back();
+//             }
+//         }
+
+//     }
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         vector<int> freq(nums.size(), 0);
+//         vector<int> temp;
+
+//         rec(ans, temp,freq, nums);
+
+//         return ans;
+//     }
+// };
